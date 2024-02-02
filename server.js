d98af8b552fe1c563f5b0446e3af27af872196d8
indlukula <84289216+indlukula@users.noreply.github.com>
2024-02-02 04:47:40 +0200

    Updated error handling

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
  * Require Statements
  *************************/
 const express = require("express")
+const utilities = require("./utilities/")
 const inventoryRoute = require("./routes/inventoryRoute")
 const baseController = require("./controllers/baseController")
 const expressLayouts = require("express-ejs-layouts")
@@ -28,7 +29,27 @@ app.use(static)
 app.use("/inv", inventoryRoute)
 
 // Index route
-app.get("/", baseController.buildHome) 
+app.get("/", utilities.handleErrors( baseController.buildHome))
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
