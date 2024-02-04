6b46f714e2197bf932ed73e299a318e99c2ce3d8
indlukula <84289216+indlukula@users.noreply.github.com>
2024-02-04 04:34:37 +0200

    created validation week4

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -17,7 +17,7 @@ validate.loginRules = () => {
       .custom(async (account_email) => {
         const emailExists = await accountModel.checkExistingEmail(
           account_email
-        );
+        )
         if (emailExists) {
           throw new Error("Email exists. Please log in or use different email")
         }
